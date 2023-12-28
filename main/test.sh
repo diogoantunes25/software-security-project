0775bbf0f490fe6836ac95963569e0d24bf33f53
Diogo Antunes <diogo.santiago.pt@gmail.com>
2023-12-28 16:49:52 +0000

    main: added support for custom testing

--- main/test.sh
+++ main/test.sh
@@ -6,25 +6,30 @@ ENDCOLOR="\e[0m"
 
 run="python3 py_analyser.py"
 
-echo "Running official tests..."
 
-folder="slices"
-for name in $(ls "$folder/" | grep py | cut -d . -f 1); do
-	echo -n "$name: "
-	script="$folder/$name.py"
-	patterns="$folder/$name.patterns.json"
-	output="$folder/$name.output.json"
-	myout="$folder/$name.my.json"
-	log="$folder/$name.log"
-	$run $script $patterns > $myout 2> $log
-	./compare $output $myout 
-	if [ $? -eq 0 ];
-	then
-		echo -e "$GREEN success $ENDCOLOR"
-	else
-		echo -e "$RED failed $ENDCOLOR"
-	fi
-done
+run_test() {
+	folder=$1
+	for name in $(ls "$folder/" | grep py | cut -d . -f 1); do
+		echo -n "$name: "
+		script="$folder/$name.py"
+		patterns="$folder/$name.patterns.json"
+		output="$folder/$name.output.json"
+		myout="$folder/$name.my.json"
+		log="$folder/$name.log"
+		$run $script $patterns > $myout 2> $log
+		./compare $output $myout 
+		if [ $? -eq 0 ];
+		then
+			echo -e "$GREEN success $ENDCOLOR"
+		else
+			echo -e "$RED failed $ENDCOLOR"
+		fi
+	done
+}
+
+echo "Running official tests..."
+run_test "slices"
 
 echo ""
 echo "Running our tests..."
+run_test "tests"
